c67871b28216e26954dda19aa2a8529bd55f5527
Matheus Malena <93771573+matheusmalena@users.noreply.github.com>
2023-10-31 00:25:54 -0300

    commit

--- ator.js
+++ ator.js
@@ -44,7 +44,7 @@ function voltaAtorPosicaoInicial() {
 function incluiPontos(){
   textAlign(CENTER);
   textSize(20);
-  fill(color(255,140,0));
+  fill(color(255,69,0));
   text(pontos, width / 2, 25);
 }
 
